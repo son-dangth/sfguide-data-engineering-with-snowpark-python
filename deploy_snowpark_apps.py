9f6517421203cf831f82fadb653148c4a93a0277
Son Dang <son_dang@epam.com>
2023-10-31 15:48:42 +0700

    Change styling

--- deploy_snowpark_apps.py
+++ deploy_snowpark_apps.py
@@ -1,7 +1,7 @@
-import sys;
-import os;
+import sys
+import os
 
-ignore_folders = ['__pycache__', '.ipynb_checkpoints']
+ignore_folders = ["__pycache__", ".ipynb_checkpoints"]
 
 if len(sys.argv) != 2:
     print("Root directory is required")
@@ -11,19 +11,19 @@ root_directory = sys.argv[1]
 print(f"Deploying all Snowpark apps in root directory {root_directory}")
 
 # Walk the entire directory structure recursively
-for (directory_path, directory_names, file_names) in os.walk(root_directory):
+for directory_path, directory_names, file_names in os.walk(root_directory):
     # Get just the last/final folder name in the directory path
     base_name = os.path.basename(directory_path)
 
     # Skip any folders we want to ignore
     if base_name in ignore_folders:
-#        print(f"Skipping ignored folder {directory_path}")
+        # print(f"Skipping ignored folder {directory_path}")
         continue
 
     # An app.toml file in the folder is our indication that this folder contains
     # a snowcli Snowpark App
     if not "app.toml" in file_names:
-#        print(f"Skipping non-app folder {directory_path}")
+        # print(f"Skipping non-app folder {directory_path}")
         continue
 
     # Next determine what type of app it is
